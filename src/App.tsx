b68782ea6c3edfba57ab04d0bdd9a9e8a53d04b6
Daniel Duarte <danrib27@hotmail.com>
2021-11-24 16:17:52 -0300

    Teste com a imagem de profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import styles from './styles/global.module.scss';
-import rocket from './assets/rocket.png';
+import profile from './assets/teste_profile.png'
+import rocket from './assets/rocket_redimentation_400h.png';
 
 export function App() {
   return (
     <main className={styles.mainContent}>
       <div className={styles.presentation}>
+        <img className={styles.profile} src={profile} />
         <h1 className={styles.welcome}><span>I'm</span> Daniel Duarte</h1>
         <img src={rocket} />
+      </div>
+      <div className={styles.separator}></div>
+      <div className={styles.info}>
+        <h1>Hello World</h1>
       </div>
     </main>
   )

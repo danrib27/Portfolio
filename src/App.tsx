ffb7e38a201699f075a5fd4b2a1de4b34dca5ecd
Daniel Duarte <danrib27@hotmail.com>
2021-11-23 22:27:54 -0300

    Imagem e apresentação

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import './styles/global.module.scss';
+import styles from './styles/global.module.scss';
+import rocket from './assets/rocket.png';
 
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <main className={styles.mainContent}>
+      <div className={styles.presentation}>
+        <h1 className={styles.welcome}><span>I'm</span> Daniel Duarte</h1>
+        <img src={rocket} />
+      </div>
+    </main>
   )
 }
